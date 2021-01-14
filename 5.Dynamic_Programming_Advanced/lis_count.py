cc02106c3e6b172f0d8cd71d81a587fdbfcad135
Seongjin Park <mememe5074@gmail.com>
2021-01-14 21:59:25 +0900

    최대 증가 부분 수열 순서대로 출력

---
+++ 5.Dynamic_Programming_Advanced/lis_count.py
@@ -0,0 +1,31 @@
+"""
+8 0
+6 2 5 1 7 4 8 3
+"""
+
+N, K = map(int, input().split())
+sequence = list(map(int, input().split()))
+cache = [-1]*N
+check = [False]*N
+new_list = []
+
+def lis(now, start):
+    if start == N-1:
+        tmp_list = []
+        tmp_list.extend(now[1:])
+        new_list.append(tmp_list)
+        tmp_list = []
+        return
+
+    for i in range(start, N):
+        start += 1
+        if now[-1] < sequence[i] and not check[i]:
+            now.append(sequence[i])
+            check[i] = True
+            lis(now, start)
+            now.pop()
+            check[i] = False
+    return
+lis([-1], 0)
+new_list.sort(key=len, reverse=True)
+print(new_list)
